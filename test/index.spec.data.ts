aefde596d3085f051d6d4dc11b01e4572f50ac52
Moustapha HappyDev <moustapha.happydev@gmail.com>
2022-08-06 16:56:14 +0000

    test: test: added mocking of fetch requests on CI environment and removed secret .env file from repo
    The docker container for Prestd binary that generates our REST Api is local to our machine, meaning
    that on CI environments on github action, we need to mock our fetch requests. That's what we've
    added on this commit

---
+++ test/index.spec.data.ts
@@ -0,0 +1,37 @@
+export const getAllMockData = [
+  {
+    id: 1,
+    firstname: 'kebe',
+    lastname: 'mouhamed',
+    phonenumber: 766942692,
+    email: 'ex@ex.com',
+    login: 'happydev',
+    password: 'QWERTY12',
+    created_at: '2022-08-03T22:06:10.838191',
+  },
+];
+
+export const getOneMockData = {
+  id: 1,
+  firstname: 'kebe',
+  lastname: 'mouhamed',
+  phonenumber: 766942692,
+  email: 'ex@ex.com',
+  login: 'happydev',
+  password: 'QWERTY12',
+  created_at: '2022-08-03T22:06:10.838191',
+};
+
+export const postMockData = {
+  id: 2,
+  firstname: 'test',
+  lastname: 'test',
+  phonenumber: 322,
+  email: 'test',
+  login: 'test',
+  password: 'test',
+};
+
+export const putMockData = { rows_affected: 1 };
+
+export const deleteMockData = { rows_affected: 1 };
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,33 +4,46 @@ import PrestApiClient from './../src/index';
 import { ApiResources, TableNames } from './typesForTests';
 import { enableFetchMocks } from 'jest-fetch-mock';
 import { config } from 'dotenv';
+import * as testData from './index.spec.data';
 config();
+// process.env.CI = 'true'; // use this line to test the behavior of tests on github actions
 
 enableFetchMocks();
-fetchMock.dontMock();
-
-let api: PrestApiClient<TableNames, ApiResources>;
-beforeEach(() => {
-  api = new PrestApiClient<TableNames, ApiResources>(
-    process.env.PRESTD_API_BASE_URL as string,
-    process.env.PRESTD_API_DATABASE_NAME as string,
-    process.env.PRESTD_API_DATABASE_SCHEMA as string
-  );
-});
 
 describe('fetching an api enpoint', () => {
+  let api: PrestApiClient<TableNames, ApiResources>;
   // describe('using getAll', () => {
+  beforeEach(() => {
+    fetchMock.resetMocks();
+
+    // Mock responses only in github actions
+    fetchMock.dontMockIf(
+      () => process.env.CI === undefined || process.env.CI === 'false'
+    );
+
+    api = new PrestApiClient<TableNames, ApiResources>(
+      process.env.PRESTD_API_BASE_URL as string,
+      process.env.PRESTD_API_DATABASE_NAME as string,
+      process.env.PRESTD_API_DATABASE_SCHEMA as string
+    );
+  });
+
   test('using getAll returns something', async () => {
+    fetchMock.mockResponse(JSON.stringify(testData.getAllMockData));
     const users = await api.getAll('users');
     expect(users).toBeDefined();
+    expect(users).toBeTruthy();
   });
 
   test('using getOne returns something', async () => {
+    fetchMock.mockResponse(JSON.stringify(testData.getOneMockData));
     const users = await api.getOne('users', 1);
     expect(users).toBeDefined();
+    expect(users).toBeTruthy();
   });
 
   test('using post returns something', async () => {
+    fetchMock.mockResponse(JSON.stringify(testData.postMockData));
     const newUser = {
       firstname: 'test',
       lastname: 'test',
@@ -39,12 +52,13 @@ describe('fetching an api enpoint', () => {
       login: 'test',
       password: 'test',
     };
-
     const data = await api.post('users', newUser);
     expect(data).toBeDefined();
+    expect(data).toBeTruthy();
   });
 
   test('using put returns something', async () => {
+    fetchMock.mockResponse(JSON.stringify(testData.putMockData));
     const newUser = {
       firstname: 'test',
       lastname: 'test',
@@ -55,11 +69,14 @@ describe('fetching an api enpoint', () => {
     };
     const data = await api.put('users', newUser, 1);
     expect(data).toBeDefined();
+    expect(data).toBeTruthy();
   });
 
   test('using delete returns something', async () => {
-    const users = await api.delete('users', 1);
-    expect(users).toBeDefined();
+    fetchMock.mockResponse(JSON.stringify(testData.deleteMockData));
+    const data = await api.delete('users', 1);
+    expect(data).toBeDefined();
+    expect(data).toBeTruthy();
   });
 
   // });
